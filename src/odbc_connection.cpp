0b1e87e861efff2a8737b50cb12fc06d1c2d55fa
srji <srji@skuniv.ac.kr>
2022-01-17 18:02:52 +0900

    altibase bigint -> numeric

--- src/odbc_connection.cpp
+++ src/odbc_connection.cpp
@@ -2914,6 +2914,7 @@ bind_buffers
       case SQL_DECIMAL:
       case SQL_FLOAT:
       case SQL_DOUBLE:
+      case SQL_BIGINT:
       case SQL_NUMERIC:
       {
         size_t character_count = column->ColumnSize + 2;
@@ -2961,14 +2962,14 @@ bind_buffers
         break;
       }
 
-      case SQL_BIGINT:
-      {
-        column->buffer_size = sizeof(SQLBIGINT);
-        column->bind_type = SQL_C_SBIGINT;
-        data->bound_columns[i].buffer =
-          new SQLBIGINT[data->fetch_size]();
-        break;
-      }
+//      case SQL_BIGINT:
+//      {
+//        column->buffer_size = sizeof(SQLBIGINT);
+//        column->bind_type = SQL_C_SBIGINT;
+//        data->bound_columns[i].buffer =
+//          new SQLBIGINT[data->fetch_size]();
+//        break;
+//      }
 
       case SQL_BINARY:
       case SQL_VARBINARY:
